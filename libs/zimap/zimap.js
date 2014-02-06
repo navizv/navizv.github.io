ce721a53c0958dd6b42ac43c616f7c0cff78dd2f
Ivan Zaycev <zaycev.ivan@gmail.com>
2014-02-06 18:54:50 +0600

    ernwrnw

--- libs/zimap/zimap.js
+++ libs/zimap/zimap.js
@@ -365,7 +365,7 @@ function Zimap(elt, opts, onload) {
 var obj = document.createElement('svg');
 obj.innerHTML = options.map.mapObject.innerHTML;
 alert(options.map.mapObject);
-options.map.mapObject.parentNode.insertBefore(options.map.mapObject, obj);
+options.map.mapObject.parentNode.append(obj);
 //                        element.parentNode.removeChild(element);
 
 options.map.mapObject
