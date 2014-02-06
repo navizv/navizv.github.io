8010110a8b9fab1cb02d058bc99c9ed99b6b1970
Ivan Zaycev <zaycev.ivan@gmail.com>
2014-02-06 18:06:47 +0600

    tmsetmse

--- libs/zimap/zimap.js
+++ libs/zimap/zimap.js
@@ -398,7 +398,7 @@ alert(reg);
     var readMap = function() {
 
         element.innerHTML = "here will be map";
-        var adr = '//navizv.github.io/libs/zimap/maps/' + options.settings.map;
+        var adr = 'http://navizv.github.io/libs/zimap/maps/' + options.settings.map;
         var svg = adr + '.svg';
         var csv = adr + '.csv';
 
