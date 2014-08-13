462ba2733464f2a9a81883701ed7c99d15f4e503
navizv <zaycev.ivan@gmail.com>
2014-08-14 02:06:16 +0600

    rustat 10%

--- rustat/db.js
+++ rustat/db.js
@@ -1,12 +1,63 @@
 function Selector() {
-    this.getRegs = function (){
+    this.getRegs = function() {
         return regs;
     }
-    this.getTree = function (){
+    this.getTree = function() {
         return regTree;
     }
     var regs = [];
     var regTree = [];
+    this.spars = [];
+    this.sregs = [];
+    this.iso = false;
+    this.clearSelect = function() {
+        this.spars = [];
+        this.sregs = [];
+    }
+    this.findISO = function(iso) {
+        for (var i in regs) {
+            var r = regs[i];
+            if (r[0] === iso)
+                return r[1];
+        }
+        return "";
+    }
+    this.selReg = function(iso) {
+        for (var i in this.sregs) {
+            var r = this.sregs[i];
+            if (r == iso)
+                return true;
+        }
+        return false;
+    }
+    var self = this;
+    this.select = function(finish) {
+        $.ajax({
+            url: this.spars[0].id,
+            dataType: "text",
+            success: function(data) {
+                var tab = [];
+                var mas = data.split("\r\n");
+                var ii=0;
+                for (var i in mas) {
+                    if (mas[i] == "")
+                        continue;
+                    var line = mas[i].split(";");
+                    if(i!=0 && !self.selReg(line[0]))
+                        continue;
+                    tab[ii] = line;
+                    if(!self.iso)
+                        tab[ii][0] = self.findISO(tab[ii][0]);
+                    for(var j in tab[ii]){
+                        if(j==0)continue;
+                        tab[ii][j] = parseFloat(tab[ii][j].replace(",",".").replace(/ /g,""));
+                    }
+                    ii++;
+                }
+                finish(tab,self.spars[0].text);
+            }
+        })
+    }
     //read iso
     $.ajax({
         url: "db/regs/iso3166.csv",
@@ -35,7 +86,7 @@ function Selector() {
                 if (mas[i] == "")
                     continue;
                 var str = mas[i].split(";");
-                if(str[0] == 1 || str[0] == 2){
+                if (str[0] == 1 || str[0] == 2) {
                     cur = regTree[ii++] = {
                         type: str[0],
                         code: str[1],
@@ -43,7 +94,7 @@ function Selector() {
                         leaves: []
                     }
                     jj = 0;
-                }else {
+                } else {
                     cur.leaves[jj++] = {
                         type: 0,
                         code: str[0],
@@ -53,5 +104,5 @@ function Selector() {
             }
         }
     });
-    
+
 }
