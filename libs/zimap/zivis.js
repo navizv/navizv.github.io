c321b742fce4ecd88188e52a4825c50a165b58bd
navizv <zaycev.ivan@gmail.com>
2019-07-31 16:09:33 +0500

    Update zivis.js

--- libs/zimap/zivis.js
+++ libs/zimap/zivis.js
@@ -140,8 +140,8 @@
                     var show_tooltip = function(tit, evt) {
                         tooltip.html(tit);
                         tooltip.css({
-                            left: evt.pageX + 5,
-                            top: evt.pageY + 35,
+                            left: evt.pageX + (options.settings.tutlex?options.settings.tutlex:5),
+                            top: evt.pageY + (options.settings.tutley?options.settings.tutley:35),
                             display: "inline"
                         });
                     }
